3ea467602f2f84b52f2b8eeffc1b3f2beeefa3bc
vishnuManimala <vishnumanimala8133@gmail.com>
2024-05-25 07:27:42 +0530

    folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { UserRegistrationComponent } from './pages/user-registration/user-registration.component';
+import { UserLoginComponent } from './pages/user-login/user-login.component';
+import { UserChatComponent } from './pages/user-chat/user-chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationComponent,
+    UserLoginComponent,
+    UserChatComponent
   ],
   imports: [
     BrowserModule,
